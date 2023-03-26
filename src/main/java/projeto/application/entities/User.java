4e1eff8de40ef52c722ed7a7b2c0aac13306df5a
D4N23 <daniilosam23@gmail.com>
2023-03-26 11:45:26 -0300

    mudança

--- src/main/java/projeto/application/entities/User.java
+++ src/main/java/projeto/application/entities/User.java
@@ -26,7 +26,7 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
-	@JsonIgnore
+	
 	@ManyToOne
 	@JoinColumn(name = "client")
 	private List<Order> orders = new ArrayList<>();
